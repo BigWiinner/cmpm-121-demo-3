cf27446883b975320f1faaf3d76345f2424da8bd
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-11-04 20:22:51 +0000

    Added button for L16 assignment

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const alertButton = document.createElement("button");
+alertButton.innerHTML = "Click me!";
+alertButton.style.backgroundColor = "pink";
+alertButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+app.append(alertButton);
